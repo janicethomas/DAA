3fa15fb368ab7e50dfe2d71565c253e9c49a8933
Janice Thomas <30435104+janicethomas@users.noreply.github.com>
2022-12-21 15:33:55 +0530

    Add files via upload

---
+++ random1.py
@@ -0,0 +1,10 @@
+import random
+n = 150000
+fp = open("random_"+str(n)+".txt","w")
+fp.write(str(n)+" ")  
+for i in range(1,n):
+    x = random.random()
+    fp.write(str(int(x*n))+" ")  
+x = random.random()
+fp.write(str(int(x*n))+" ")  
+fp.close()
